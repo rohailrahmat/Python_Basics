1f4d813f463839aac67f91ff50774bcbbbdb690b
Rohail Rahmat <150139278+rohailrahmat@users.noreply.github.com>
2025-05-22 00:13:09 -0700

    Added function practice code

---
+++ day_3.py
@@ -0,0 +1,73 @@
+# let start with some new things buddy like data types in python
+# 1. Data Types
+# 2. Variables
+name = "my name is rohail rahmat baig"
+print(name)
+
+father_name ='my father name is Rahmat baig'
+print(father_name)
+
+
+birthday = "i was born on 29th of march 2004 in barkolti yasin"
+print(birthday)
+
+# 3. Lists
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+my_favorite_foods =["biryani", "karahi", "pulao", "chicken", "mutton","Bindi"]
+print(my_favorite_foods)
+# 4. Integers
+my_age = 21
+print("MY AGE IS:",my_age)
+my_marks_in_math = 90
+print("in 10TH class my marks in math was :",my_marks_in_math)
+#floats 
+my_height = 5.9
+print("my height in feets",my_height)
+the_Value_of_pie = 3.14
+print("the value of pie is",the_Value_of_pie)
+
+# 4. Tuples
+my_tuple = (1, 2, 3, 4, 5, 6,)
+print(my_tuple)
+tuple_of_my_fav_furits = ("apple","mango", "banana","orange","grapes")
+print(tuple_of_my_fav_furits)
+
+# 5. Dictionaries
+my_dict ={
+    "name": "rohail rahmat ",
+    "age": 21,
+    "height": 5.5,
+    "weight": 60,
+    "hobbies": ["coding","designing","singing","playing football"],
+}
+print(my_dict)
+
+# 6. Sets
+my_set = {1, 2, 3, 4, 5, 6}
+print(my_set)
+My_fav_stars = {"shah ruhk khan", "akhsay Kumar", "tiger shrof"}
+print("These are my favorite film stars:",My_fav_stars)
+# 7. Strings
+what_are_you_learing = "iam learing python and i am going to master it at any cost"
+print("this is a string",what_are_you_learing)
+
+#BOOLEAN
+iam_a_student = True
+print("iam a students:",iam_a_student)
+
+#none
+iam_a_student = None
+print("iam a student:",iam_a_student)
+
+# 8. Functions
+def my_function():
+    print("this is a function in python")
+    my_function()
+
+def greet_user(name):
+    print(f"Hello, {name}!")
+    greet_user("Rohail Rahmat Baig")
+    
+
+
