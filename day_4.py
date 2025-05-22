a7c3d547070965c76e676520b3711565aa344a95
Rohail Rahmat <150139278+rohailrahmat@users.noreply.github.com>
2025-05-22 06:18:57 -0700

    code is updated

---
+++ day_4.py
@@ -0,0 +1,11 @@
+# conditional statements
+total_bill = 990 
+discount = 10 
+if total_bill > 100:
+    print("the bill is greater then your got a discount of 10%")
+    total_bill = total_bill - (total_bill * discount / 100)
+    print(total_bill)
+
+else: 
+    print("the bill is less then 100 no discout")
+    print("your total bill is:", total_bill)
